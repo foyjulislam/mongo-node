dd630fffc70f5e8cd2f3565608308e37a4bf8a9d
foyjulislam <foyjulislam.syl20@gmail.com>
2020-04-10 19:42:15 +0600

    doctors portal api committed

--- index.js
+++ index.js
@@ -11,141 +11,75 @@ app.use(bodyParser.json());
 
 // dbUser & pass ata always hide . 
 const uri = process.env.DB_PATH;
-
 let client = new MongoClient(uri, { useNewUrlParser: true });
 
-const users = ['arif', 'jamil', 'Kamal', 'Jamal', 'Rohim', 'Karim'];
+
+//get data from database
 
 
-// database connection
+// services from doctorsPortal database
 
-// mongodb+srv://dbUserFoyjul:<password>@cluster0-fesfb.mongodb.net/test?retryWrites=true&w=majority
-
-
-app.get('/products', (req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("onlineStore").collection("products");
-        collection.find().toArray((err, documents) => {
-             if (err) {
-                 console.log(err)
-                 res.status(500).send({message:err});   
-             } else {
-                res.send(documents);
-             }
-        });
-      //console.log('Database Connected...')
-        client.close();
+app.get("/services", (req, res) => {
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("doctorsPortal").collection("services");
+    collection.find().toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(documents);
+      }
     });
-})
+    client.close();
+  });
+});
 
-app.get('/orders', (req, res) => {
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("onlineStore").collection("orders");
-        collection.find().toArray((err, documents) => {
-             if (err) {
-                 console.log(err)
-                 res.status(500).send({message:err});   
-             } else {
-                res.send(documents);
-             }
-        });
-      //console.log('Database Connected...')
-        client.close();
+
+
+app.get("/dailyAppointment/:appointmentDate", (req, res) => {
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("doctorsPortal").collection("appointments");
+    collection.find(req.params).toArray((err, documents) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(documents);
+      }
     });
-})
+    client.close();
+  });
+});
 
-// app.get('/fruits/banana', (req, res) =>{
-//     res.send({fruit:'banana', quantity:1000, price:10000});
+// closed get data code
 
-// })
 
-// single get product
-app.get('/product/:key', (req, res) => {
-    const key = req.params.key;
-    //console.log(req.query.sort);
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("onlineStore").collection("products");
-        collection.find({key: key}).toArray((err, documents) => {
-             if (err) {
-                 console.log(err)
-                 res.status(500).send({message:err});   
-             } else {
-                res.send(documents[0]);
-             }
-        });
-      //console.log('Database Connected...')
-        client.close();
+//post data to database
+
+
+app.post("/bookAppointment", (req, res) => {
+  const appointmentDetails = req.body;
+  appointmentDetails.bookingDate = new Date();
+  client = new MongoClient(uri, { useNewUrlParser: true });
+  client.connect((error) => {
+    const collection = client.db("doctorsPortal").collection("appointments");
+    collection.insertOne(appointmentDetails, (err, result) => {
+      if (err) {
+        console.log(err);
+        console.log(error)
+        res.status(500).send({ message: err });
+      } else {
+        res.send(result.ops[0]);
+      }
     });
-})
+    client.close();
+  });
+});
 
-app.post('/getProductsByKey', (req, res) => {
-    const key = req.params.key;
-    const productKeys = req.body;
-    console.log(productKeys);
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("onlineStore").collection("products");
-        collection.find({key: { $in: productKeys}}).toArray((err, documents) => {
-             if (err) {
-                 console.log(err)
-                 res.status(500).send({message:err});   
-             } else {
-                res.send(documents);
-             }
-        });
-      //console.log('Database Connected...')
-        client.close();
-    });
-})
-
-
-//post
-
-app.post('/addProduct', (req, res) => {
-    
-//save to database
-    const product = req.body;
-    client = new MongoClient(uri, { useNewUrlParser: true });
-    client.connect(err => {
-        const collection = client.db("onlineStore").collection("products");
-        collection.insert(product, (err, result) => {
-             if (err) {
-                 console.log(err)
-                 res.status(500).send({message:err});   
-             } else {
-                res.send(result.ops[0]);
-             }
-        });
-      //console.log('Database Connected...')
-        client.close();
-      });
-
-})
-
-app.post('/placeOrder', (req, res) => {
-  const orderDetails = req.body;
-    orderDetails.orderTime  = new Date();
-    console.log(orderDetails);
-
-        client = new MongoClient(uri, { useNewUrlParser: true });
-        client.connect(err => {
-            const collection = client.db("onlineStore").collection("orders");
-            collection.insertOne(orderDetails , (err, result) => {
-                 if (err) {
-                     console.log(err)
-                     res.status(500).send({message:err});   
-                 } else {
-                    res.send(result.ops[0]);
-                 }
-            });
-          //console.log('Database Connected...')
-            client.close();
-        });
-    
-})
 
 const port = process.env.PORT || 4200;
 app.listen(port, () => console.log('Listening to port 4200'));
